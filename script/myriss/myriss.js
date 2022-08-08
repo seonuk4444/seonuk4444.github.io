e86399cf636931ba9d11abc7ada7ae4a778fbe25
seonuk4444 <109269386+seonuk4444@users.noreply.github.com>
2022-08-08 09:00:41 +0900

    Add files via upload

---
+++ script/myriss/myriss.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+    $('.optBtn1').click(function(e){
+        e.preventDefault();
+        if($(this).hasClass('on')){
+          $(this).removeClass('on');
+          $(this).next('.optType1').slideUp();
+        }else{
+          $(this).addClass('on');
+          $(this).next('.optType1').slideDown();
+        }
+      });
+
+    $(".shelf .favorRadio[name='shelf']:radio").change(function () {
+        $('.shelf').removeClass('mainShelf')
+       $(this).parents('.shelf').addClass('mainShelf');         
+    });
+
+    //내 책장수정 팝업
+    $('.shelfBtns .edit').click(function(e){
+        e.preventDefault();
+        let ph = $('.myPop .popHeader').outerHeight(true);
+        $('.myPop .popCont').css({'height':'calc(100% - ' +ph+'px)'})
+        $('.blackBg1').show();
+        $('.myPop').addClass('on');
+    });
+
+    $('.myPop .divPopupClose').click(function(e){
+        e.preventDefault();
+        $('.blackBg1').hide();
+        $('.myPop').removeClass('on');
+    });
+
+    //카테고리 팝업
+    $('#interestCodeName1').click(function(){
+        let ph = $('.categoryPop .popHeader').outerHeight(true);
+        $('.categoryPop .popCont').css({'height':'calc(100% - ' +ph+'px)'})
+        $('.blackBg1').show();
+        $('.categoryPop').addClass('on');
+    });
+    $('.categoryPopClose').click(function(e){
+        e.preventDefault();
+        $('.categoryPop').removeClass('on');
+    });
+
+    //내보내기
+  $('.export').click(function(e){
+    e.preventDefault();
+    let ph = $('.exportPop .popHeader').outerHeight(true);
+    $('.exportPop .popCont').css({'height':'calc(100% - ' +ph+'px)'})
+    $('.blackBg1').show();
+    $('.exportPop').addClass('on');
+  });
+  $('.exportPopClose').click(function(e){
+    e.preventDefault();
+    $('.blackBg1').hide();
+    $('.exportPop').removeClass('on');
+  });
+
+
+  //월간이용현황
+  let graphTab = new Swiper('.graphTab .inner',{
+    slidesPerView:'auto',
+    spaceBetween: 0,
+    freeMode: true,
+  });
+});    
